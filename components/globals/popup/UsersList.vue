df1784480ebbd91dacba2b1a80514de8ffc34a5b
BadBonus <pikavitik@gmail.com>
2025-11-12 22:19:54 +0300

    upd: сформирован хедер чата

---
+++ components/globals/popup/UsersList.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+type TPopupUser = {
+  avatar: string;
+  slug: string;
+};
+
+defineOptions({
+  name: "PopupCUsersList",
+});
+
+defineProps<{
+  users: TPopupUser[];
+}>();
+</script>
+
+<template>
+  <UPopover
+    :content="{
+      align: 'center',
+    }"
+  >
+    <slot />
+
+    <template #content>
+      <ul class="flex w-fit max-w-[195px] flex-wrap gap-1.5 p-1.5">
+        <li :key="u.slug" v-for="u in users">
+          <NuxtLink :to="'/profile/' + u.slug">
+            <UAvatar :src="u.avatar" class="border-border size-9 border-1" />
+          </NuxtLink>
+        </li>
+      </ul>
+    </template>
+  </UPopover>
+</template>
--- pages/Chat/[id].vue
+++ pages/Chat/[id].vue
@@ -2,10 +2,57 @@
 defineOptions({
   name: "Chat_id",
 });
+
+const demoUsers = [
+  {
+    avatar: "/images/wod_1.png",
+    slug: "blablabla",
+  },
+  {
+    avatar: "/images/wod_1.png",
+    slug: "blablabla2",
+  },
+  {
+    avatar: "/images/wod_1.png",
+    slug: "blablabla3",
+  },
+  {
+    avatar: "/images/wod_1.png",
+    slug: "blablabl4",
+  },
+  {
+    avatar: "/images/wod_1.png",
+    slug: "blablabla5",
+  },
+  {
+    avatar: "/images/wod_1.png",
+    slug: "blablabl6a",
+  },
+];
 </script>
 
 <template>
-  <div class="Chat_id">123</div>
+  <section class="Chat_id">
+    <header>
+      <NuxtLink to="/chat">
+        <Button
+          onlyIcon
+          icon="material-symbols:keyboard-return-rounded"
+          class="mx-auto"
+        />
+      </NuxtLink>
+      <h1 class="mb-0 text-center text-2xl font-semibold">
+        GM Rah - Bear and Артур Есипов
+      </h1>
+
+      <PopupUsersList :users="demoUsers">
+        <button class="mx-auto flex items-center text-3xl">
+          <Icon name="ic:baseline-people-outline" />
+          <span class="text-2xl">:22</span>
+        </button>
+      </PopupUsersList>
+    </header>
+  </section>
 </template>
 
 <!-- <style lang="scss"></style> -->
