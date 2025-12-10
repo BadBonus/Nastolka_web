41e5d115da124101a864a05de054d96bcd3dfd5d
BadBonus <pikavitik@gmail.com>
2025-12-10 23:15:06 +0300

    design: добавил текстуры

--- components/features/main/Footer.vue
+++ components/features/main/Footer.vue
@@ -4,18 +4,25 @@ defineOptions({
 });
 </script>
 <template>
-  <footer class="bg-bg-footer border-t-brown w-full border-t-4 p-6">
-    <div class="text-inverted flex items-center gap-1 text-3xl">
+  <footer class="bg-bg-footer border-t-brown relative w-full border-t-4 p-6">
+    <div class="text-inverted relative z-10 flex items-center gap-1 text-3xl">
       <span class="font-decorate font-bold"> NASTOLKA </span>
       <Icon name="game-icons:cubes" />
     </div>
+    <div class="MainFooter__decore"></div>
   </footer>
 </template>
 
-<!-- <style scoped>
-footer {
-  box-shadow:
-    inset 0 0.5em 1.5em #0000001a,
-    inset 0 0.125em 0.5em #00000026;
+<style scoped>
+.MainFooter__decore {
+  width: 100%;
+  height: 103%;
+  background-repeat: repeat;
+  background-image: url("/images/bg_decors/wood.webp");
+  position: absolute;
+  left: 0;
+  top: -3%;
+  z-index: 0;
+  opacity: 0.4;
 }
-</style> -->
+</style>
--- components/features/main/Header/index.vue
+++ components/features/main/Header/index.vue
@@ -10,16 +10,33 @@ const authStore = useAuthStore();
 </script>
 <template>
   <header
-    class="bg-bg-header border-b-brown flex h-16 w-full items-center justify-between border-b-4 px-4"
+    class="MainHeader bg-bg-header border-b-brown relative h-16 border-b-4 px-4"
   >
-    <NuxtLink to="/" class="text-inverted flex items-center gap-1 text-3xl">
-      <span class="font-decorate font-bold max-md:hidden"> NASTOLKA </span>
-      <Icon name="game-icons:cubes" />
-    </NuxtLink>
+    <div class="MainHeader__decore"></div>
+    <div class="relative z-10 flex h-full w-full items-center justify-between">
+      <NuxtLink to="/" class="text-inverted flex items-center gap-1 text-3xl">
+        <span class="font-decorate font-bold max-md:hidden"> NASTOLKA </span>
+        <Icon name="game-icons:cubes" />
+      </NuxtLink>
 
-    <div class="flex items-center gap-3">
-      <NotificationPopup />
-      <Drawer :isAuth="authStore.isAuthenticated" />
+      <div class="flex items-center gap-3">
+        <NotificationPopup />
+        <Drawer :isAuth="authStore.isAuthenticated" />
+      </div>
     </div>
   </header>
 </template>
+
+<style>
+.MainHeader__decore {
+  width: 100%;
+  height: 103%;
+  background-repeat: repeat;
+  background-image: url("/images/bg_decors/wood.webp");
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 0;
+  opacity: 0.4;
+}
+</style>
