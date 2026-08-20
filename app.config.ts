a96679b982eb5e139d97ad28a0e8859d636aecd4
ArturYes <pikavitik@gmail.com>
2026-08-21 00:32:12 +0300

    UPD: внёс новые утилиты-бордеры для "рисованной" стилистики

--- app.config.ts
+++ app.config.ts
@@ -18,5 +18,17 @@ export default defineAppConfig({
         base: 'border-2',
       },
     },
+    button: {
+      variants: {
+        color: {
+          error: 'sketchy-border-3',
+          info: 'sketchy-border-4',
+          success: 'sketchy-border-5',
+          primary: 'sketchy-border-2',
+          warning: 'sketchy-border',
+          secondary: 'sketchy-border-6',
+        },
+      },
+    },
   },
 });
